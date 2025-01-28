721c466ea87cd80171ea4428c497dcaa1ea3b115
nekena123 <rakotomanjatonekena@gmail.com>
2025-01-28 11:11:14 +0300

    test

--- crypto/src/main/java/com/cloud/crypto/mvtFond/MvtFondController.java
+++ crypto/src/main/java/com/cloud/crypto/mvtFond/MvtFondController.java
@@ -3,16 +3,20 @@ package com.cloud.crypto.mvtFond;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
+@RequestMapping("/api/MvtFond")
+@CrossOrigin(origins = "http://localhost:8081")
 public class MvtFondController {
-    // @Autowired
-    // private MvtFondService mvtFondService;
+    @Autowired
+    private MvtFondService mvtFondService;
 
     @GetMapping("/insertMvt")
     public ModelAndView insertMvt(@RequestParam int idUtilisateur,
