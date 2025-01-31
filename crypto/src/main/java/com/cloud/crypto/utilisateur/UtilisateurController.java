fdf804a9567a738b14fc7ca4e338efe17b4899bf
nekena123 <rakotomanjatonekena@gmail.com>
2025-01-31 11:23:46 +0300

    Update UtilisateurController.java

--- crypto/src/main/java/com/cloud/crypto/utilisateur/UtilisateurController.java
+++ crypto/src/main/java/com/cloud/crypto/utilisateur/UtilisateurController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/crypto/api/utilisateurs")
+@RequestMapping("/api/utilisateurs")
 public class UtilisateurController {
     @Autowired
     private UtilisateurService utilisateurService;
