fbbc79434bb0833f544fc0d20981a708313a228e
sergianafr <sergiravelo@gmail.com>
2025-01-31 11:09:40 +0300

    Update UtilisateurController.java

--- crypto/src/main/java/com/cloud/crypto/utilisateur/UtilisateurController.java
+++ crypto/src/main/java/com/cloud/crypto/utilisateur/UtilisateurController.java
@@ -1,37 +1,25 @@
 package com.cloud.crypto.utilisateur;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/crypto/api")
+@RequestMapping("/crypto/api/utilisateurs")
 public class UtilisateurController {
+    @Autowired
+    private UtilisateurService utilisateurService;
     
     @GetMapping("/test")
     public ResponseEntity<String> test(){
         return ResponseEntity.ok("Mandeha ilay izy");
     }
-}
-package com.cloud.crypto.utilisateur;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-@RestController
-@RequestMapping("/api/utilisateurs")
-public class UtilisateurController {
-    @Autowired
-    private UtilisateurService utilisateurService;
-
+    
      @GetMapping("/")
     public ResponseEntity<?> getUtilisateurByEmail(@RequestParam(name = "email") String email) {
         Optional<Utilisateur> utilisateur = utilisateurService.getUtilisateurByEmail(email);
@@ -41,5 +29,4 @@ public class UtilisateurController {
             return ResponseEntity.status(404).body("Utilisateur non trouvé");
         }
     }
-    
 }
