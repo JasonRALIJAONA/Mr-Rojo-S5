ecf923c21a8bdc017f1bf189ff8d7cf991908ca9
nekena123 <rakotomanjatonekena@gmail.com>
2025-01-31 18:26:12 +0300

    docker

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,3 +1,17 @@
+CREATE DATABASE fournisseur_identite;
+\c fournisseur_identite;
+
+CREATE TABLE utilisateur(
+    id serial PRIMARY KEY, 
+    nom_utilisateur varchar(50) unique,
+    email varchar(50) unique,
+    mot_de_passe varchar(50) not null,
+    est_valide boolean default false,
+    nb_tentative integer default 0
+);
+
+\c postgres;
+
 CREATE DATABASE crypto;
 \c crypto;
 
