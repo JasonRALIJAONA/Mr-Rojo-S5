2e9fd347091d96bc0f3b76fdfad1d4d461b0cbc9
nekena123 <rakotomanjatonekena@gmail.com>
2025-02-01 19:29:43 +0300

    .

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE utilisateur(
     id serial PRIMARY KEY, 
     nom_utilisateur varchar(50) unique,
     email varchar(50) unique,
-    mot_de_passe varchar(50) not null,
+    mot_de_passe varchar(100) not null,
     est_valide boolean default false,
     nb_tentative integer default 0
 );
--- fournisseurIdentite/Models/FournisseurIdentiteContext.cs
+++ fournisseurIdentite/Models/FournisseurIdentiteContext.cs
@@ -19,7 +19,7 @@ public partial class FournisseurIdentiteContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=fournisseur_identite;Username=postgres;Password=admin");
+        => optionsBuilder.UseNpgsql("Host=postgres;Port=5432;Database=fournisseur_identite;Username=postgres;Password=admin");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
