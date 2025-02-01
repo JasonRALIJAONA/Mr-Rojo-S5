f27f20a05090d13ae554a58121527a3bcfc73814
nekena123 <rakotomanjatonekena@gmail.com>
2025-02-01 19:49:03 +0300

    cors

--- fournisseurIdentite/Program.cs
+++ fournisseurIdentite/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:80")  // Frontend autorisé
+            policy.WithOrigins("http://localhost:3000")  // Frontend autorisé
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials(); // Si vous avez besoin des cookies ou des sessions
