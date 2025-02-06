69428569163fcce445513638583720cc412fed18
Jason Andriniaina RALIJAONA <jasonralijaona@gmail.com>
2025-02-06 15:18:48 +0300

    commentaire

--- crypto/src/main/java/com/cloud/crypto/firebase/SyncService.java
+++ crypto/src/main/java/com/cloud/crypto/firebase/SyncService.java
@@ -31,7 +31,7 @@ public class SyncService {
         this.firestore = firestore;
     }
 
-    @Scheduled(fixedRate = 60000) // Sync every 1 minute
+    // @Scheduled(fixedRate = 60000) // Sync every 1 minute
     public void syncAllTablesToFirestore() {
         entityManager.getMetamodel().getEntities().forEach(entity -> {
             try {
