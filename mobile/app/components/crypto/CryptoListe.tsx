fe0c5948ea57810a888e809fcdf98bb8ac31302f
sergianafr <sergiravelo@gmail.com>
2025-02-07 20:08:49 +0300

    test integration firebase

--- mobile/app/components/crypto/CryptoListe.tsx
+++ mobile/app/components/crypto/CryptoListe.tsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import CryptoCard from './CryptoCard';
-import axios from "axios";
+import { db } from '../../../firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
-const baseUrl = 'http://localhost:8080/api';
-let cards = [];
+export default function CryptoListe() {
+    const [cards, setCards] = useState([]);
 
-  // Invoking the get method to perform a GET request
-axios.get(`${baseUrl}/cryptos`).then((response) => {
-    cards = response.data;
-    console.log(response.data);
-});
+    useEffect(() => {
+        const fetchCryptos = async () => {
+            const querySnapshot = await getDocs(collection(db, "cryptomonnaie"));
+            const cryptos = [];
+            querySnapshot.forEach((doc) => {
+                cryptos.push({ id: doc.id, ...doc.data() });
+            });
+            setCards(cryptos);
+        };
 
-export default function CryptoListe(){
+        fetchCryptos();
+    }, []);
+
     return (
-        <SafeAreaView style={{flex:1}}>
-            {/* <View style={styles.headerContainer}>
-                <Text style={styles.header}>Liste des cryptos</Text>
-            </View> */}
+        <SafeAreaView style={{ flex: 1 }}>
             <FlatList
                 data={cards}
-                keyExtractor={(item)=>item.id}
-                renderItem={({item}) => (
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
                     <CryptoCard
                         symbole={item.symbole}
                         nom={item.nom}
@@ -30,26 +34,25 @@ export default function CryptoListe(){
                         buttonText=""
                     />
                 )}
-                numColumns={3} 
-                contentContainerStyle={styles.cardsContainer}>
-            </FlatList>
+                numColumns={3}
+                contentContainerStyle={styles.cardsContainer}
+            />
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     headerContainer: {
-      padding: 16,
-      backgroundColor: '#0F2573',
+        padding: 16,
+        backgroundColor: '#0F2573',
     },
     header: {
-      fontSize: 24,
-      color: '#fff',
-      fontWeight: 'bold',
-      textAlign: 'center',
+        fontSize: 24,
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
     cardsContainer: {
-      padding: 16,
+        padding: 16,
     },
-  });
-  
+});
